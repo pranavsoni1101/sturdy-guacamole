01389a7dc0e21954061a282f7983c16ee1e3ea65
Pranav Soni <pranavsoni1101@gmail.com>
2023-04-17 10:35:06 +0530

    Added custom theme to the website

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,12 +5,13 @@ import Layout from "../../component/Layout";
 // import theme from "../theme/theme";
 
 import "../styles/globals.css";
+import theme from "../../theme/theme";
 
 const App = ({ Component, pageProps }: AppProps): ReactNode => {
 
     return (
 
-        <ChakraProvider >
+        <ChakraProvider theme={theme} >
             <CSSReset />
             <Layout>
                 <Component {...pageProps} />
---
+++ theme/theme.ts
@@ -0,0 +1,13 @@
+import { extendTheme } from "@chakra-ui/react";
+
+
+const theme = extendTheme({
+    colors: {
+        brand1: {
+            500: "#007f82"
+            
+        }
+    }
+})
+
+export default theme;
