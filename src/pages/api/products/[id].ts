014dab9d547869d7ddcf820b5590f4a8a4db13f6
Pranav Soni <pranavsoni1101@gmail.com>
2023-05-08 16:55:16 +0530

    Added productType page to use database

--- src/pages/api/products/[id].ts
+++ src/pages/api/products/[id].ts
@@ -3,10 +3,13 @@ import clientPromise from "../../../../lib/mongodb";
 const handler = async (req: any, res: any) => {
     const client = await clientPromise;
     const db = client.db(process.env.DB_NAME);
+    console.log("query herer heheheheh",req.query.id);
+    const id = req.query.id
+    
     switch (req.method) {
         case "GET":
-        const allProducts = await db.collection("allProducts").findOne();
-        res.json({ status: 200, data: allProducts });
+        const singleProduct = await db.collection("allProducts").findOne({label : id});
+        res.json({ status: 200, data: singleProduct });
         break;
     }
 }
--- src/pages/ourProducts/[productType]/index.tsx
+++ src/pages/ourProducts/[productType]/index.tsx
@@ -3,81 +3,44 @@ import { Heading, Text, Box, Grid, GridItem, Image, Button } from '@chakra-ui/re
 import Section from '../../../../component/Section';
 import SectionContent from '../../../../component/Section/SectionContent';
 import { useRouter } from 'next/router';
-import data from '../data';
 import Link from 'next/link';
 import Head from 'next/head';
-import path from 'path';
-import fs from 'fs';
-import fsPromises from "fs/promises";
-import allProducts from '../../../../backendData/data.json';
 
-import { GetStaticPaths, GetStaticProps } from 'next';
-import matter from 'gray-matter';
+import { GetServerSideProps } from 'next';
 
-interface PropsInterface {
-    label: string | undefined;
-	img: string | undefined;
-	img2?: string;
-	subLabel?: string;
-	children?: Array<PropsInterface>;
-}
 
-const ProductType = (props: { data: PropsInterface, hasError: boolean }) => {
+const ProductType = (props: any) => {
 
     const router  = useRouter();
     const { productType } = router.query!;
-    console.log("router as path",router.asPath);
-    
-    
-    const [state, setState]= useState({});
-    console.log("state",state);
-    
+    console.log("this is the props",props.singleProduct.data.childrenProduct);
 
-        console.log("this is the props",props);
-    useEffect(() => {
-        let windowStorage = window.sessionStorage.getItem("data");
-        setState(JSON.parse(windowStorage!));
-        window.sessionStorage.setItem("data", JSON.stringify(props));
-
-    //     allProducts.products.map((product) => {
-    //         if(productType === product.label){
-    //             setState(prevState => [
-    //                 ...prevState,
-    //                 product
-    //             ]);
-    //         }
-    //     })
-
-    }, [])
-
+    const {label, subLabel, img, childrenProduct} = props.singleProduct.data
     return(
         <>
             <Head>
-                {/* <title>{state.data.label}</title> */}
+                <title>{label}</title>
             </Head>
             <Section>
                 <SectionContent>
                     <Heading>
-                        {/* {props.data.label} */}
+                        {label}
                     </Heading>
                     <Text
                         my = "1em"
                     >
-                        {/* {state[0].subLabel} */}
-                        {/* {props.data.subLabel} */}
+                        {subLabel}
                     </Text>
                     <Grid
                         templateColumns = "repeat(12, 1fr)"
                         gap={4}
                     >
-                        {/* {props.data.children?.map(child => ( */}
-                        {/* {state[0].children!.map((child: any) => ( */}
+                        {childrenProduct.map((child: any) => (
                             <GridItem
                                 colSpan={3}
                             >
                                 <Link
-                                    // href={`/ourProducts/${productType}/${child.label}`}
-                                    href={`/ourProducts/${productType}/product  `}
+                                    href={`/ourProducts/${productType}/${child.label}`}
                                 >
                                 <Box
                                     borderRadius= "3xl"
@@ -85,7 +48,7 @@ const ProductType = (props: { data: PropsInterface, hasError: boolean }) => {
                                 >
                                     <Image 
                                         borderRadius= "20px 20px 0  0"
-                                        src='https://images.unsplash.com/photo-1681725142773-5756fad63e93?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'
+                                        src = {`${child.img}`}
                                     />
                                     <Box
                                         p = "1em"
@@ -95,13 +58,13 @@ const ProductType = (props: { data: PropsInterface, hasError: boolean }) => {
                                             as = "h4"
                                             fontSize= "md"
                                         >
-                                            {/* {child.label} */}
+                                            {child.label}
                                         </Heading>
                                     </Box>
                                 </Box>
                                 </Link>
                             </GridItem>
-                        {/* ))}  */}
+                        ))}
                     </Grid>
                 </SectionContent>
             </Section>
@@ -111,72 +74,19 @@ const ProductType = (props: { data: PropsInterface, hasError: boolean }) => {
 
 export default ProductType;
 
-async function getData() {
-    // const filePath = path.join(process.cwd(), 'backendData','data.json');
-    // const jsonData = await fsPromises.readFile(filePath);
-    // const data = JSON.parse(jsonData.toString());
-  
-    const fullPath = path.join(process.cwd(), 'backendData', 'data.json');
-    const fileContents = fs.readFileSync(fullPath, 'utf8');
-  
-    // Use gray-matter to parse the post metadata section
-    const data = JSON.parse(fileContents);
-  
-    return data;
-  }
+  export const getServerSideProps: GetServerSideProps = async (context) => {
+    const {productType} = context.query;
+    let res = await fetch(`http://localhost:3000/api/products/${productType}`, {
+        method: "GET",
+        headers: {
+            "Content-type": "application/json"
+        }
+    })
 
-  export const getStaticProps: GetStaticProps = async (context) => {
-    const prodLabel = context.params?.productType;
-    const data = await getData();
-    const foundItem = data.products.find((item: PropsInterface) => prodLabel === item.label);
-  
-    if (!foundItem) {
-      return {
-        props: { hasError: true },
-      }
-  }
-  
-  return {
-    props: {
-        productData: foundItem
-    }
-  }
-}
-
-
-//   export const getStaticProps: GetStaticProps = async (context) => {
-//     const productLabel = context.params?.productType;
-//     let data, foundItem;
-//     try{
-//         data = await getData();        
-//         foundItem = data.products.find((item: PropsInterface) => productLabel === item.label);
-//     }
-//     catch(err){
-//         throw err;
-//     }
-//     if (!foundItem) {
-//       return {
-//         props: { hasError: true },
-//       }
-//   }
-//   console.log("found item at",foundItem)
-//   return {
-//     props: {
-//       data: foundItem
-//     }
-//   }
-// }
-
-export const getStaticPaths: GetStaticPaths = async () => {
-    const data = await getData();
-    // console.log("Data",JSON.parse(data));
-    
-    const pathsWithParams = data.products.map((star: PropsInterface) => ({ params: { productType: star.label }}))
-
-    // const pathsWithParams = data.data.map((star: PropsInterface) => ({ params: { productType: star.label }}))
+    let singleProduct = await res.json();
 
     return {
-        paths: [],
-        fallback: true
+      props: {singleProduct}
     }
   }
+  
